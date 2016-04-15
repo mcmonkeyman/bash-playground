eb14a3ae5a669caae5de924a7535c199ea720f7f
Eoin O'Connor <eoconnor@reonomy.com>
2016-04-15 15:06:35 -0400

    Add chronos backup script

--- backup-chronos.sh
+++ backup-chronos.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 # requires jsonpp to be installed
 
+echo ${CHRONOS_USER}:?"Need to set CHRONOS_USER to non-empty value"}
+echo ${CHRONOS_PASS}:?"Need to set CHRONOS_PASS to non-empty value"}
+
+if [ $# -ne 1 ]; then
+  echo "Missing env."
+  echo "Usage:./backup-chronos.sh dev|prd"
+  exit 1
+fi
+
+ENV=$1
+
 function timestamp {
   return date +"%T"
 }
 
-CHRONOS_URL="http://chronos.dev.reonomy.com"
-CHRONOS_USER=
-CHRONOS_PASS=
+CHRONOS_URL="http://chronos.$ENV.reonomy.com"
 
-BACKUP_DIR="./backup/"
+BACKUP_DIR="./chronos-backups/"
 mkdir -p ${BACKUP_DIR}
-BACKUP_FILE=${BACKUP_DIR}dev_backup_$(date +%Y-%m-%d--%H%M%S).json
+BACKUP_FILE=${BACKUP_DIR}${ENV}_backup_$(date +%Y-%m-%d--%H%M%S).json
 
 curl -v  -H "Content-Type:application/json"  -u $CHRONOS_USER:$CHRONOS_PASS $CHRONOS_URL/scheduler/jobs | jsonpp > ${BACKUP_FILE} 
